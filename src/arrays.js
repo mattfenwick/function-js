773bfeba205f849426b39621e2d4d491dfe2bb4a
Matt Fenwick <mfenwick100@gmail.com>
2014-03-07 08:04:27 -0500

    more array functions; move folds out of main and into arrays module

--- src/arrays.js
+++ src/arrays.js
@@ -107,6 +107,59 @@ function all() {
     return true;
 }
 
+function zip(xs, ys) {
+    // this could be generalized to any number of arrays
+    var end = (xs.length < ys.length) ? xs.length : ys.length;
+    var out = [];
+    for (var i = 0; i < end; i++) {
+        out.push([xs[i], ys[i]);
+    }
+    return out;
+}
+
+/*
+ there's no way both `foldr` and `foldl` are right ... is there?
+ gotta check.  haskell:
+ foldr :: (a -> b -> b) -> b -> [a] -> b
+ foldr f b [] = b
+ foldr f b (x:xs) = foldr f (f x b) xs
+ foldr f b (x:xs) = f x (foldr f b xs) -- <-- is it this one?
+ 
+ foldr (:) [8] [1,2] = foldr (:) [1,8] [2]
+                     = foldr (:) [2,1,8] []
+                     = [2,1,8] ??????????
+ foldr (:) [8] [1,2] = ??????? second way
+
+ foldl :: (b -> a -> b) -> b -> [a] -> b
+ foldl f b [] = b
+ foldl f b (x:xs) = foldl f (f b x) xs
+ foldl f b (x:xs) = f (foldl f b xs) x -- <-- is it this one?
+*/
+function foldr(f, b, xs) {
+    // (a -> b -> b) -> b -> [a] -> b
+    // foldr (+) 0 [1,2,3,4,5] =>
+    //   ((((0+1)+2)+3)+4)+5
+    if ( xs.length === 0 ) {
+        return b;
+    }
+    return foldr(f, f(xs[0], b), xs.slice(1));
+    // would be better to do this w/o recursion
+}
+
+function foldl(f, b, xs) {
+    // (b -> a -> b) -> b -> [a] -> b
+    // foldl (+) 0 [1,2,3,4,5] => 
+    //   0+(1+(2+(3+(4+5))))
+    if ( xs.length === 0 ) {
+        return b;
+    }
+    return f(foldl(f, b, xs.slice(1)), xs[0]);
+}
+
+function group() {
+    throw new Error('oops');
+}
+
 
 module.exports = {
     'getArgs' :  getArgs,
@@ -121,6 +174,9 @@ module.exports = {
     'map'     :  map,
     'filter'  :  filter,
     'any'     :  any,
-    'all'     :  all
+    'all'     :  all,
+    'zip'     :  zip,
+    'foldr'   :  foldr,
+    'foldl'   :  foldl,
+    'group'   :  group
 };
-
